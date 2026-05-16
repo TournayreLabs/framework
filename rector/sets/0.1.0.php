187ba52f4d4accf085b3c065addadb0c9ad57de0
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2026-05-17 00:32:57 +0200

    chore(rector): set 0.1.0 — ajouter renommage setLoggerIdentifier → identifiedAs

--- rector/sets/0.1.0.php
+++ rector/sets/0.1.0.php
@@ -10,5 +10,9 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->ruleWithConfiguration(RenameMethodRector::class, [
         // AbstractLogger::getLoggerIdentifier() → loggerIdentifier() (EO: no getters)
         new MethodCallRename('TournayreLabs\Common\Log\AbstractLogger', 'getLoggerIdentifier', 'loggerIdentifier'),
+        // AbstractLogger::setLoggerIdentifier() → identifiedAs() (EO: no setters)
+        new MethodCallRename('TournayreLabs\Common\Log\AbstractLogger', 'setLoggerIdentifier', 'identifiedAs'),
+        // LoggerInterface::setLoggerIdentifier() → identifiedAs() (EO: no setters)
+        new MethodCallRename('TournayreLabs\Contracts\Log\LoggerInterface', 'setLoggerIdentifier', 'identifiedAs'),
     ]);
 };
